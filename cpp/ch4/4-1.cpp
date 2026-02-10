a2dd6a32a6f753908760bce544a7ac288e6c0ef3
epqlffltm <kjh55984249@gmail.com>
2026-02-10 17:26:27 +0900

    class

---
+++ cpp/ch4/4-1.cpp
@@ -0,0 +1,43 @@
+/*
+2026-02-10
+class
+*/
+#include<iostream>
+
+class Animal
+{
+  private:
+  int food;
+  int weight;
+
+
+  public:
+  void set_animal(int _food, int _weight)
+  {
+    food = _food;
+    weight = _weight;
+  }
+
+  void increase_food(int inc)
+  {
+    food += inc;
+    weight += (inc/3);
+  }
+
+  void view_stat()
+  {
+    std::cout << "food: "<<food<<std::endl;
+    std::cout << "weigth: "<<weight<<std::endl;
+  }
+};
+
+int main(void)
+{
+  Animal animal;
+  animal.set_animal(100,50);
+  animal.increase_food(30);
+
+  animal.view_stat();
+
+  return 0;
+}
