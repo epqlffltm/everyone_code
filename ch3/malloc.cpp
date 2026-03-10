036e092a59283358ac18a8364ec54713c2debb14
epqlffltm <kjh55984249@gmail.com>
2026-03-10 16:34:05 +0900

    malloc

---
+++ ch3/malloc.cpp
@@ -0,0 +1,31 @@
+/*
+2026-03-10
+동적 할당
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+int main(void)
+{
+  strad(time(NULL));
+  int size = 0;
+  std::cout<< "arrray size :";
+  std::cin >> size;
+
+  int *ary = new int[size];
+  for(int i = 0; i < size; i++)
+  {
+    *(ary+i) = rand()%10;
+  }
+
+  for(int i =0; i<size; i++)
+  {
+    std::cout << *(ary+i)<<std::endl;
+  }
+
+  delete ary;
+
+  return 0;
+}
