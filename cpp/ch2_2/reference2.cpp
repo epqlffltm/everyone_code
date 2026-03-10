68146d63274be30c5c93ad204fc041627c949507
epqlffltm <kjh55984249@gmail.com>
2026-03-10 14:57:07 +0900

    reference2

---
+++ cpp/ch2_2/reference2.cpp
@@ -0,0 +1,38 @@
+/*
+2026-03-10
+레퍼런스
+*/
+
+#include<iostream>
+
+int val(int *p);
+int rev(int &r);
+
+int main(void)
+{
+  int n = 5;
+  int& r = n;
+
+  std::cout << "n: " << n << std::endl;
+  std::cout << "r: "<< r << std::endl;
+  int pn = val(&n);
+  int rn = rev(n);
+  std::cout << "pn: " << std::endl;
+  std::cout << "rn: " << std::endl;
+
+  return 0;
+}
+
+int val(int *p)
+{
+  int n = *p + 5;
+
+  return n;
+}
+
+int rev(int &r)
+{
+  int n = &r + 10;
+
+  return n;
+}
