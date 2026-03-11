10d55ca4a1e4cab8b3e3bdfe9379c723a3b9797d
epqlffltm <kjh55984249@gmail.com>
2026-03-11 17:12:41 +0900

    map

---
+++ cpp/ch10/map.cpp
@@ -0,0 +1,34 @@
+/*
+2026-03-11
+map
+*/
+
+#include<iostream>
+#include<map>
+#include<string>
+
+template<typename k, typename v>
+void print(std::map<k, v>&m);
+
+int main(void)
+{
+  std::map<std::string, double>m;
+
+  m.insert(std::pair<std::string, double>("pi",3.14));
+  print(m);
+  m.insert({"golden ratio",1.168});
+  print(m);
+  m["golden ratio"]=40000;
+  print(m);
+
+  return 0;
+} 
+
+template<typename k, typename v>
+void print(std::map<k, v>&m)
+{
+  for(auro itr = m.begin(); itr != m.end()l ++itr)
+  {
+    std::cout << itr->first << "  " << itr->second << std::endl;
+  }
+}
